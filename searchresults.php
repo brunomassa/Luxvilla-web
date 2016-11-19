47277c893464ebf1d2d17ca66f668e0c096f98d2
brunomassa <massa_ferreira@hotmail.com>
2016-11-19 20:18:32 +0000

    added search function
    now can search hauses in website

--- searchresults.php
+++ searchresults.php
@@ -2,13 +2,14 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Lux Villa</title>
+<title>Resultados da pesquisa</title>
 <link href="styles/styles.css" rel="stylesheet">
 </head>
 
 <body bgcolor="#CCC">
+<div style="min-height:100px;">
 <ul id="menu">
-  <li style="float:left"><a href="#">Lux Villa</a></li>
+  <li style="float:left"><a href="http://brunomassa.esy.es/">Lux Villa</a></li>
   <li><a href="sobre nos.html">Sobre n&oacute;s</a></li>
   <li><a href="http://brunomassa.esy.es/#casas">Casas</a></li>
   
@@ -19,47 +20,15 @@
 </form>
  
 </ul>
-
-<link href="cssslider_files/csss_engine1/style.css" rel="stylesheet">
-
-<script src="cssslider_files/csss_engine1/gestures.js" type="text/javascript"></script> <div class='csslider1 autoplay '>
-		<input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide' >
-		<input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide' >
-		<input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide' >
-		<input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked>
-		<input name="cs_anchor1" id='cs_pause1_0' type="radio" class='cs_anchor pause'>
-		<input name="cs_anchor1" id='cs_pause1_1' type="radio" class='cs_anchor pause'>
-		<input name="cs_anchor1" id='cs_pause1_2' type="radio" class='cs_anchor pause'>
-		<ul>
-			<li class="cs_skeleton"><img style="width: 100%;" src="casa3.jpg"></li>
-			<li class='num0 img slide'>  <a href="casa1.html" target="_self"><img src="casa1.jpg" alt='Braga' title='Braga' /> </a> </li>
-			<li class='num1 img slide'>  <a href="casa8.html" target="_self"><img src="casa8.jpg" alt='Porto' title='Porto' /> </a> </li>
-			<li class='num2 img slide'>  <a href="casa10.html" target="_self"><img src="casa10.jpg" alt='Porto' title='Porto' /> </a> </li>
-		</ul><div class="cs_engine"></div>
-		<div class='cs_description'>
-			<label class='num0'><span class="cs_title"><span class="cs_wrapper">1.100.000 &euro;</span></span></label>
-			<label class='num1'><span class="cs_title"><span class="cs_wrapper">3.200.000 &euro;</span></span></label>
-			<label class='num2'><span class="cs_title"><span class="cs_wrapper">1.600.000 &euro;</span></span></label>
-		</div>
-		<div class='cs_arrowprev'>
-			<label class='num0' for='cs_slide1_0'><span><i></i><b></b></span></label>
-			<label class='num1' for='cs_slide1_1'><span><i></i><b></b></span></label>
-			<label class='num2' for='cs_slide1_2'><span><i></i><b></b></span></label>
-		</div>
-		<div class='cs_arrownext'>
-			<label class='num0' for='cs_slide1_0'><span><i></i><b></b></span></label>
-			<label class='num1' for='cs_slide1_1'><span><i></i><b></b></span></label>
-			<label class='num2' for='cs_slide1_2'><span><i></i><b></b></span></label>
-		</div>
-		</div>
+</div>
 <div style="background:#CCC; width:100%; min-height:100px; margin-top:10px; padding-bottom:10px; overflow: hidden;" id="casas">
-
 <?php
 define("servername","mysql.hostinger.pt");
 define("username","u460723307_massa");
 define("password","marianaraiodesol");
 define("dbname","u460723307_app");
 
+$query=$_POST["q"];
 
 // Create connection
 
@@ -84,14 +53,18 @@ $stmt->bind_result($id, $imgurl, $local, $preco, $infocasa, $linkcasa);
     
 while($stmt->fetch()) 
 {
-	 /*echo " - Name: " . $row["local"]. " " . $row["preco"]. "<br>";*/
+	$locallower=strtolower($local);
+	$precolower=strtolower($preco);
+	$infocasalower=strtolower($infocasa);
+	$querylower=strtolower($query);
 	
-    echo '<div class="caixacasas">
+	 if(strpos($locallower,$querylower)!==false || strpos($precolower,$querylower)!==false || strpos($infocasalower,$querylower) !==false){
+		 echo '<div class="caixacasassearchresults">
           <div id="imagem" style="display:inline-block;"><a href="'. $linkcasa.'">
-          <img width="250px" height="200px" src="'.$imgurl.'" class="imagenscaixas"></a>
-          <p style="margin-top: 10px !important;">'. $local.'</p>'.
-          '<p style="margin-top: 10px !important;">'. $preco.'</p>
-          </div></div>';
+          <img width="250px" height="200px" src="'.$imgurl.'" class="imagenscaixassearchresult"></a>
+          <p style="margin-top: 10px !important; float:right;">'. $local.'<br><br>'.$preco.'</p>'.
+          '</div></div>';
+	 }
 }
 
 $stmt->close();
@@ -111,6 +84,5 @@ mysqli_close($conn);
 </svg><font size="5" color="#FFFFFF"> github (Android)</font></span></button></a>
         </div>
 </footer>
-
 </body>
 </html>
